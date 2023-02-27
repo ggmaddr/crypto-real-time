284d74e9354c98a2396f7d15b929df12825a51b1
ggmaddr <ggiangta@gmail.com>
2023-02-26 22:02:34 -0800

    apiruls

---
+++ client/src/assets/apiURLs.js
@@ -0,0 +1,33 @@
+const apiURLs = [
+    {
+        //bitcoin, binancecoin, staked-ether, unidex, ripple, euler, neo, dash
+        name:"Favorite",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20binancecoin%2C%20staked-ether%2C%20unidex%2C%20ripple%2C%20euler%2C%20neo%2C%20dash&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d"
+    },
+    {
+        //category_id: decentralized-finance-defi
+        name:"DeFi",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=decentralized-finance-defi&order=market_cap_desc&per_page=8&page=1&sparkline=false"
+    },
+    {
+        //category_id: governance
+        name:"Governance",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=governance&order=market_cap_desc&per_page=8&page=1&sparkline=false"
+    },
+    {
+        //category_id: governance
+        name:"Governance",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=governance&order=market_cap_desc&per_page=8&page=1&sparkline=false"
+    },
+    {
+        //category_id: governance
+        name:"Governance",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=governance&order=market_cap_desc&per_page=8&page=1&sparkline=false"
+    },
+    {
+        //category_id: governance
+        name:"Governance",
+        url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=governance&order=market_cap_desc&per_page=8&page=1&sparkline=false"
+    }
+]
+export default apiURLs;
--- client/src/components/priceCard.js
+++ client/src/components/priceCard.js
@@ -1,28 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
-
+import apiURLs from '../assets/apiURLs'
 const PriceCard = (props) =>{
     const category = props.category;
     const [coinData, setCoinData] = useState(null)
     let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20binancecoin%2C%20staked-ether%2C%20unidex%2C%20ripple%2C%20euler%2C%20neo%2C%20dash&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d"
-    const apiURLs = [
-        {
-            //bitcoin, binancecoin, staked-ether, unidex, ripple, euler, neo, dash
-            name:"Favorite",
-            url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20binancecoin%2C%20staked-ether%2C%20unidex%2C%20ripple%2C%20euler%2C%20neo%2C%20dash&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d"
-        },
-        {
-            //category_id: decentralized-finance-defi
-            name:"DeFi",
-            url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=decentralized-finance-defi&order=market_cap_desc&per_page=8&page=1&sparkline=false"
-        },
-        {
-            //category_id: governance
-            name:"Governance",
-            url:"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=governance&order=market_cap_desc&per_page=8&page=1&sparkline=false"
-        }
-    ]
+    
     const findAPI = ()=>{
         for (let apiURL of apiURLs)
         {
@@ -44,7 +28,7 @@ const PriceCard = (props) =>{
             console.log(err)
         })
     },[category]) //re-render when "Category" updates
-    
+
     if (!coinData) return null
 
     return (
